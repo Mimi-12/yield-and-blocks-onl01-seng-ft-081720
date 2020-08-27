132f273fce5a352dd8c97e477874cae8a0bd2b84
selam lemma <oneiconforlife26@gmail.com>
2020-08-27 13:23:47 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,8 @@
-#def hello_t(array)
- #i = 0
+def hello_t(array)
+  i = 0
 
- #while i < array.length
-  #   yield array[i]
-    # i = i + 1
-#   end
-end
-
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+  while i < array.length
+    yield array[i]
+    i = i + 1
   end
 end
